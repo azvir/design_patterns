48071f22a4372011ba3e04232120acc261e0f9e9
Andrei Bondar <andrei@qtf.io>
2022-11-17 15:53:59 +0100

    Singleton metaclass

---
+++ creational_patterns/singleton/3_singleton_metaclass.py
@@ -0,0 +1,20 @@
+class Singleton(type):
+    __instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls.__instances:
+            cls.__instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls.__instances[cls]
+
+
+class Database(metaclass=Singleton):
+    def __init__(self):
+        print('Loading database')
+
+
+if __name__ == '__main__':
+    d1 = Database()
+    d2 = Database()
+    print(d1 == d2)
+
+
